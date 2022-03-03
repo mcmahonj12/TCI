ab50077600a05c82cdab2d32bd88bc593a046370
Rick Rolle <rickrolle@rolled.com>
2022-03-03 14:24:59 -0800

    new local permissions

--- scripts/Config-vCenter.ps1
+++ scripts/Config-vCenter.ps1
@@ -1,5 +1,7 @@
-
 $vCenter = "vc-mkt-4.dishwasher.cloud"
+$rolePath = ".\config\global\vc\roles\*.json"
+$idPath = ".\config\global\vc\idsrc\idsources.json"
+$permsPath = ".\config\local\vc\permissions\vc-mkt-4-perms.json"
 $creds = Get-Credential
 
 #Connect to the target vCenter Server
@@ -8,12 +10,12 @@ Connect-VIServer -server $vCenter -Credential $creds
 # Import the VIRole sciprt as a module
 Import-Module -Name ".\scripts\Import-VIRole.ps1"
 
-Import-VIRole -RolePath ".\config\global\vc\roles\*.json"
+Import-VIRole -RolePath $rolePath
 Pause
 
-.\scripts\Add-VcIdSource.ps1 -ViServer $vcenter -Credential $creds -FilePath ".\config\global\vc\idsrc\idsources.json"
+.\scripts\Add-VcIdSource.ps1 -ViServer $vcenter -Credential $creds -FilePath $idPath
 Pause
 
-.\scripts\Add-VIPermissions.ps1 -FilePath ".\config\global\vc\permissions\globalPermissions.json"
+.\scripts\Add-VIPermissions.ps1 -FilePath $permsPath
 
 Disconnect-VIServer -Confirm:$false
