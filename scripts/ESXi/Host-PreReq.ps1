95b03f7fde98dd8312c5fd718eff88f61e113861
Jim McMahon II <mcmahonj@vmware.com>
2022-05-20 18:13:38 -0400

    NTP check

--- scripts/ESXi/Host-PreReq.ps1
+++ scripts/ESXi/Host-PreReq.ps1
@@ -72,17 +72,37 @@ Write-Host "Attempting to connect to host" -ForegroundColor Green
     }
 
 # Check NTP is configured, the service is running, and the NTP servers are reachable.
-    # Check the SSH service policy is set to start and stop with the host.
-    Write-Host "Checking the SSH service is enabled. If not, enabling." -ForegroundColor Green
+    # Check NTP is configured with time servers.
+    Write-Host "Checking if time servers are configured on the ESXi host. If not, set them." -ForegroundColor Green
     try {
-        If ($ssh = Get-VMHostService | Where-Object {($_.Key -eq "TSM-ssh") -and ($_.Policy -ne "on")}) {
+        $currNtp = Get-VMHostNtpServer
+        If (!($currNtp)) {
             Write-Host "Setting SSH policy to 'Start and stop with host'" -ForegroundColor Magenta
-            Set-VMHostService $ssh -policy "on" -Confirm:$false | Restart-VMHostService -Confirm:$false
+            $ntp | ForEach-Object {Add-VMHostNtpServer -NTPServer $_}
+        }
+        ElseIf ($currNtp -ne $ntp) {
+            Get-VMHostNtpServer | ForEach-Object {Remove-VMHostNtpServer -NtpServer $_ -Confirm:$false}
+            $ntp | Foreach-Object {Add-VMHostNtpServer -NtpServer $_ -Confirm:$false}
         }
     }
     catch {
-        Write-Host "HOST_CHECK_HOST_SSH"
-        Write-Host "Could not change the SSH host policy to 'Start and stop with host'"
+        Write-Host "HOST_CHECK_NTP_CONFIG"
+        Write-Host "Could not add NTP servers."
+        $error[0]
+    }
+
+    # Check NTP is configured with time servers.
+    Write-Host "Enable the NTP service." -ForegroundColor Green
+    try {
+        If ($ntpSvc = Get-VMHostService | Where-Object {($_.Key -eq "ntpd") -and ($_.Running -ne $True)}) {
+            Write-Host "The SSH service is not running. Starting the SSH service." -ForegroundColor Magenta
+            Start-VMHostService $ntpSvc -Confirm:$false
+        }
+    }
+    catch {
+        Write-Host "HOST_CHECK_NTP_CONFIG"
+        Write-Host "Could not enable and start the NTP service."
+        $error[0]
     }
 
 # Check if the ESXi host is in Maintenance Mode. If so, try to exit Maintenance Mode.
@@ -95,10 +115,9 @@ Write-Host "Attempting to connect to host" -ForegroundColor Green
         }
     }
     catch {
-        Write-Host "HOST_CHECK_HOST_SSH"
-        Write-Host "Could not change the SSH host policy to 'Start and stop with host'"
+        Write-Host "HOST_CHECK_IN_MAINTENANCE"
+        Write-Host "The host could not be taken out of Maintenance Mode."
+        Write-Host $error[0]
     }  
 
 }
-
-HOST_CHECK_SERVER_VENDOR
