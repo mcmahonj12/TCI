e30c403acd352aae44c99077aa2629b04ccab2de
Jim McMahon II <mcmahonj@vmware.com>
2022-05-20 12:36:57 -0400

    added ntp, ssh checks

--- scripts/ESXi/Host-PreReq.ps1
+++ scripts/ESXi/Host-PreReq.ps1
@@ -43,4 +43,48 @@ Write-Host "Attempting to connect to host" -ForegroundColor Green
         Write-Host "Unable to connect to host $_ via API."       
     }
 
+# Check SSH is enabled
+    # Check the SSH service policy is set to start and stop with the host.
+    Write-Host "Checking the SSH service is enabled. If not, enabling." -ForegroundColor Green
+    try {
+        If ($ssh = Get-VMHostService | Where-Object {($_.Key -eq "TSM-ssh") -and ($_.Policy -ne "on")}) {
+            Write-Host "Setting SSH policy to 'Start and stop with host'" -ForegroundColor Magenta
+            Set-VMHostService $ssh -policy "on" -Confirm:$false | Restart-VMHostService -Confirm:$false
+        }
+    }
+    catch {
+        Write-Host "HOST_CHECK_HOST_SSH"
+        Write-Host "Could not change the SSH host policy to 'Start and stop with host'"
+    }
+
+    # Check the SSH service is running and start it if not. 
+    Write-Host "Checking the SSH service is running. If not, starting." -ForegroundColor Green   
+    try {
+        If ($ssh = Get-VMHostService | Where-Object {($_.Key -eq "TSM-ssh") -and ($_.Running -ne $True)}) {
+            Write-Host "The SSH service is not running. Starting the SSH service." -ForegroundColor Magenta
+            Start-VMHostService $ssh -Confirm:$false
+        }
+    }
+    catch {
+        Write-Host "HOST_CHECK_HOST_SSH"
+        Write-Host "Could not start the SSH service."
+        $error[0]
+    }
+
+# Check NTP is configured, the service is running, and the NTP servers are reachable.
+    # Check the SSH service policy is set to start and stop with the host.
+    Write-Host "Checking the SSH service is enabled. If not, enabling." -ForegroundColor Green
+    try {
+        If ($ssh = Get-VMHostService | Where-Object {($_.Key -eq "TSM-ssh") -and ($_.Policy -ne "on")}) {
+            Write-Host "Setting SSH policy to 'Start and stop with host'" -ForegroundColor Magenta
+            Set-VMHostService $ssh -policy "on" -Confirm:$false | Restart-VMHostService -Confirm:$false
+        }
+    }
+    catch {
+        Write-Host "HOST_CHECK_HOST_SSH"
+        Write-Host "Could not change the SSH host policy to 'Start and stop with host'"
+    }
+
 }
+
+HOST_CHECK_SERVER_VENDOR
