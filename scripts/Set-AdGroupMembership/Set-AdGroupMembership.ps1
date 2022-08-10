ac57e393d969ce3226ca3f1285bc69d7af2442a1
mcmahonj12 <mcmahonj@vmware.com>
2022-08-09 23:45:14 -0400

    added groups automation

--- scripts/Set-AdGroupMembership/Set-AdGroupMembership.ps1
+++ scripts/Set-AdGroupMembership/Set-AdGroupMembership.ps1
@@ -1,13 +1,10 @@
-function Add-ADOUs {
+﻿function Set-AdGroupMembership {
     <#
     .SYNOPSIS
-    This PowerCLI cmdlet creates an Active Directory structure to include OUs via Active Directory.
+    This PowerCLI cmdlet creates sets Active Directory group membership in bulk.
     
     .DESCRIPTION
-    This function creates an Active Directory structure defined in a CSV to to prepare OUs
-    for VMWare cloud architectures. The deployment structure can be modified by changing the data in the CSV file. Using a CSV file 
-    for large deployments is the desireable method as multiple changes may be made using Excel rather than scrolling through YAML/JSON
-    to make changes. This is especially helpful in the case of lab structures repeatedly recreated.
+    This function bulk sets Active Directory user group membership as specified in the groups.json file. 
 
     The Microsoft Active Directory PowerShell module is required to use this script. To install the Active Directory module open
     a PowerShell command window as Administrator and run "Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0."
@@ -17,10 +14,13 @@ function Add-ADOUs {
     
     .LINK
     This function was created and tested using Microsoft Windows Server 2022 with a Domain Level of 2016.
+
+    .EXAMPLE
+    Set-AdGroupMembership -ADServer 172.16.0.10 -Credential (Get-Credential) -JSONPath C:\Configs\groups.json
     
-    .PARAMETER CSVPath
-    CSVPath: string
-    Set the location of the CSV where AD OU settings are defined.
+    .PARAMETER JSONPath
+    JSONPath: string
+    Set the location of the JSON where AD group settings are defined.
     #>
 
     
@@ -34,18 +34,21 @@ function Add-ADOUs {
         [ValidateNotNullorEmpty()]
         [Alias("Creds")]
         [pscredential]$Credential,
-        [Parameter(Mandatory = $true, Position = 1, HelpMessage = 'Path to the CSV file describing the OUs')]
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = 'Path to the JSON file describing the group configurations')]
         [ValidateNotNullorEmpty()]
         [Alias("Path")]
-        [String]$CSVPath
+        [String]$JSONPath
     )
 
     try {
-        $ous = Import-CSV -Path $CSVPath
+        $groups = Get-Content $JSONPath | ConvertFrom-JSON
     }
     catch {
         throw $error[0].Exception.Message
     }
 
-    $ous | ForEach-Object {New-ADOrganizationalUnit -Server $server -Credential $creds -Name $_.name -Path $_.path}
+    $groups | ForEach-Object { if ($_.members){
+            Add-ADGroupMember -Server $ADServer -Credential $creds -Identity $_.Name -Members $_.members
+        }
+    }
 }
